823742c35223451355949e5e6caac9d2c8a67c48
Christoph Grabo <chris@dinarrr.com>
2014-02-16 17:58:43 +0100

    Reduce spec code (DRY)

--- spec/sjekksum_spec.rb
+++ spec/sjekksum_spec.rb
@@ -2,106 +2,33 @@ require "spec_helper"
 
 describe Sjekksum do
 
+  spec_input  = 193435600
+
   spec_values = {
-    input:       12345,
     damm:        9,
     luhn:        5,
-    upc:         7,
-    verhoeff:    1,
-    primitive:   6,
-    primitive97: 8
+    upc:         3,
+    verhoeff:    4,
+    primitive:   4,
+    primitive97: 7
   }
 
   specify { expect(described_class).to be_a(Module) }
 
-  it "#damm" do
-    expected = spec_values[:input] * 10 + spec_values[:damm]
-    expect(described_class.damm(spec_values[:input])).to eq(spec_values[:damm])
-  end
+  spec_values.each do |algo, check|
+    final_value = check.is_a?(String) ? "#{spec_input}#{check}" : spec_input * 10 + check
 
-  it "#damm?" do
-    value = spec_values[:input] * 10 + spec_values[:damm]
-    expect(described_class.damm?(value)).to be_true
-  end
+    it "##{algo}" do
+      expect(described_class.send(algo, spec_input)).to eq(check)
+    end
 
-  it "#damm!" do
-    expected = spec_values[:input] * 10 + spec_values[:damm]
-    expect(described_class.damm!(spec_values[:input])).to eq(expected)
-  end
+    it "##{algo}?" do
+      expect(described_class.send(:"#{algo}?", final_value)).to be_true
+    end
 
-  it "#luhn" do
-    expected = spec_values[:input] * 10 + spec_values[:luhn]
-    expect(described_class.luhn(spec_values[:input])).to eq(spec_values[:luhn])
-  end
-
-  it "#luhn?" do
-    value = spec_values[:input] * 10 + spec_values[:luhn]
-    expect(described_class.luhn?(value)).to be_true
-  end
-
-  it "#luhn!" do
-    expected = spec_values[:input] * 10 + spec_values[:luhn]
-    expect(described_class.luhn!(spec_values[:input])).to eq(expected)
-  end
-
-  it "#upc" do
-    expected = spec_values[:input] * 10 + spec_values[:upc]
-    expect(described_class.upc(spec_values[:input])).to eq(spec_values[:upc])
-  end
-
-  it "#upc?" do
-    value = spec_values[:input] * 10 + spec_values[:upc]
-    expect(described_class.upc?(value)).to be_true
-  end
-
-  it "#upc!" do
-    expected = spec_values[:input] * 10 + spec_values[:upc]
-    expect(described_class.upc!(spec_values[:input])).to eq(expected)
-  end
-
-  it "#verhoeff" do
-    expected = spec_values[:input] * 10 + spec_values[:verhoeff]
-    expect(described_class.verhoeff(spec_values[:input])).to eq(spec_values[:verhoeff])
-  end
-
-  it "#verhoeff?" do
-    value = spec_values[:input] * 10 + spec_values[:verhoeff]
-    expect(described_class.verhoeff?(value)).to be_true
-  end
-
-  it "#verhoeff!" do
-    expected = spec_values[:input] * 10 + spec_values[:verhoeff]
-    expect(described_class.verhoeff!(spec_values[:input])).to eq(expected)
-  end
-
-  it "#primitive" do
-    expected = spec_values[:input] * 10 + spec_values[:primitive]
-    expect(described_class.primitive(spec_values[:input])).to eq(spec_values[:primitive])
-  end
-
-  it "#primitive?" do
-    value = spec_values[:input] * 10 + spec_values[:primitive]
-    expect(described_class.primitive?(value)).to be_true
-  end
-
-  it "#primitive!" do
-    expected = spec_values[:input] * 10 + spec_values[:primitive]
-    expect(described_class.primitive!(spec_values[:input])).to eq(expected)
-  end
-
-  it "#primitive97" do
-    expected = spec_values[:input] * 10 + spec_values[:primitive97]
-    expect(described_class.primitive97(spec_values[:input])).to eq(spec_values[:primitive97])
-  end
-
-  it "#primitive97?" do
-    value = spec_values[:input] * 10 + spec_values[:primitive97]
-    expect(described_class.primitive97?(value)).to be_true
-  end
-
-  it "#primitive97!" do
-    expected = spec_values[:input] * 10 + spec_values[:primitive97]
-    expect(described_class.primitive97!(spec_values[:input])).to eq(expected)
+    it "##{algo}!" do
+      expect(described_class.send(:"#{algo}!", spec_input)).to eq(final_value)
+    end
   end
 
 end
